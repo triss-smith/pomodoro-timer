34c6ead547a83830eb359b82a2c5509a2ea52ff2
triss-smith <smith.triston@gmail.com>
2021-02-26 03:59:41 -0600

    Updates

---
+++ precache-manifest.d067b64d7a088bda2f58f36e8ff9ed76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bd6a4b4c059dd02b6bfb85ff983239",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "5982e727020c3ecfccd4",
+    "url": "/pomodoro-timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/pomodoro-timer/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro-timer/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5982e727020c3ecfccd4",
+    "url": "/pomodoro-timer/static/js/main.2f5d25e9.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
